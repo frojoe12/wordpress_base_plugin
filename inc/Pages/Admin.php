398798b816f975e340ed9bffbfef8e55bf3b3f93
Joe Frost <cex@cexs-mac-mini.home>
2020-12-30 19:19:47 +0000

    update admin pages array

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -27,7 +27,7 @@ class Admin extends BaseController {
                 'callback' => function() {echo '<h1>Joe</h1>';},
                 'icon_url' => 'dashicons-store',
                 'position' => 110
-            ]
+            ],
         ];
 
 
